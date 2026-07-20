76d3f6c0f7f5ad7755486706e7da337842d96f58
Nethaka Haldo <nhaldo@ece006.ece.local.cmu.edu>
2026-07-20 16:22:50 -0400

    wrote divider rtl

--- rtl/divider.sv
+++ rtl/divider.sv
@@ -9,8 +9,8 @@ module divider #(
     input logic [WIDTH-1:0] divisor,
 
     output logic busy,
-    output logic done
-    output logic [WIDTH+FRAC_BITS:0] quotient,
+    output logic done,
+    output logic [WIDTH+FRAC_BITS-1:0] quotient,
     output logic [WIDTH-1:0] remainder,
     output logic dbz
 );
@@ -18,28 +18,87 @@ module divider #(
 localparam N = WIDTH + FRAC_BITS;
 
 typedef enum logic [1:0] {
-    IDLE = 2'b00,
-    CALC = 2'b01,
-    DONE = 2'b10,
+    IDLE_STATE = 2'b00,
+    CALC_STATE = 2'b01,
+    DONE_STATE = 2'b10
 } state_t;
 
-state_t curr_state;
+state_t state;
+
+logic [WIDTH:0] acc; // to hold answer + scratch for running partial remainder
+logic [$clog2(N+1)-1:0] i; // iteration count
+
+logic [N-1:0] dvd; // input to consume MSB by MSB
+logic [N-1:0] quo; // output to build LSB by LSB
+logic [WIDTH-1:0] dvsr_l; // latched divisor
+logic dbz_l; // latched divide-by-zero flag
 
 always_ff @(posedge clk) begin
     if (rst) begin
-        blah;
+        state <= IDLE_STATE;
+        busy <= '0;
+        done <= '0;
+
+        acc <= '0;
+        dbz <= '0;
+        quotient <= '0;
+        remainder <= '0;
+
     end else begin
-        case (curr_state)
-            IDLE: begin
-                blah;
+        case (state)
+            IDLE_STATE: begin
+                if (start) begin
+                    dvd <= dividend << FRAC_BITS;
+                    dvsr_l <= divisor;
+                    dbz_l <= (divisor == 0);
+
+                    acc <= '0;
+                    quo <= '0;
+                    i <= '0;
+
+                    busy <= 1'b1;
+                    state <= CALC_STATE;
+                end
             end
 
-            CALC: begin
-                blah;
+            CALC_STATE: begin
+                automatic logic [WIDTH:0] acc_shifted = {acc[WIDTH-1:0], dvd[N-1]};
+
+                dvd <= dvd << 1;
+
+                if (acc_shifted >= dvsr_l) begin // divisible
+                    acc <= acc_shifted - dvsr_l;
+                    quo <= {quo[N-2:0], 1'b1};
+                end else begin // not divisible
+                    acc <= acc_shifted;
+                    quo <= {quo[N-2:0], 1'b0};
+                end
+
+                if (i == N-1) begin
+                    state <= DONE_STATE;
+                end
+
+                i <= i + 1;
             end
 
-            DONE: begin
-                blah;
+            DONE_STATE: begin
+                busy <= 1'b0;
+                done <= 1'b1;
+                dbz <= dbz_l;
+
+                if (dbz_l) begin
+                    quotient <= '0;
+                    remainder <= '0;
+                end else begin
+                    quotient <= quo;
+                    remainder <= acc[WIDTH-1:0];
+                end
+
+                state <= IDLE_STATE;
+            end
+
+            default: begin
+                state <= IDLE_STATE;
             end
         endcase
     end
